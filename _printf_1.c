4d3c76721428c7d5399e3df65376e4fd3d115340
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-19 03:20:07 -0700

    add more comments

--- _printf_1.c
+++ _printf_1.c
@@ -8,7 +8,7 @@
 
 int _printf(const char *format, ...)
 {
-	int totalchar = 0, count;
+	int totalchar = 0, count; /* totalchar: monitors len of printf string */
 	char ch, *str;
 	va_list var;
 
@@ -25,19 +25,19 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
-			if (*format == '\0')
+			if (*format == '\0') /* end program if a null byte is encountered */
 				break;
 			if (*format == 'c')
 			{
-				ch = va_arg(var, int);
+				ch = va_arg(var, int); /* ch copies content of va_arg at that index */
 				write(1, &ch, 1);
 				totalchar++;
 			}
 			if (*format == 's')
 			{
-				str = va_arg(var, char *);
+				str = va_arg(var, char *); /* str copies va_arg */
 				count = strlen(str);
-				write(1, str, count);
+				write(1, str, count); /* 1: stdout, str: what to print, count: bytes */
 				totalchar = totalchar + count;
 			}
 			if (*format == '%')
